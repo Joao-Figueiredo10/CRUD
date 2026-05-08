28129b56686d97ca97eea6724870c7d7c1fe8722
Joao-Figueiredo10 <a14871@oficina.pt>
2026-05-08 16:03:24 +0000

    commit

--- Crud/appdb (1).sql
+++ Crud/appdb (1).sql
@@ -3,10 +3,11 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: db
--- Tempo de geração: 08-Maio-2026 às 15:42
+-- Tempo de geração: 08-Maio-2026 às 16:02
 -- Versão do servidor: 8.4.9
 -- versão do PHP: 8.3.26
 
+SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -32,6 +33,7 @@ USE `appdb`;
 DROP TABLE IF EXISTS `clientes`;
 CREATE TABLE `clientes` (
   `id` int NOT NULL,
+  `nif` varchar(15) NOT NULL,
   `nome` varchar(100) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
   `telefone` varchar(20) DEFAULT NULL
@@ -41,9 +43,9 @@ CREATE TABLE `clientes` (
 -- Extraindo dados da tabela `clientes`
 --
 
-INSERT INTO `clientes` (`id`, `nome`, `email`, `telefone`) VALUES
-(1, 'Joao', 'abc@sapo.pt', '123456789'),
-(2, 'Luis', 'abc@sapo.pt', '12345');
+INSERT INTO `clientes` (`id`, `nif`, `nome`, `email`, `telefone`) VALUES
+(1, '', 'Joao', 'abc@sapo.pt', '123456789'),
+(2, '', 'Luis', 'abc@sapo.pt', '12345');
 
 --
 -- Índices para tabelas despejadas
@@ -64,6 +66,7 @@ ALTER TABLE `clientes`
 --
 ALTER TABLE `clientes`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+SET FOREIGN_KEY_CHECKS=1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
